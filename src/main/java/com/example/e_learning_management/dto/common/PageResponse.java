947503079f6331e6ff2f867053fcf630bfb4cace
Thomas Phong <ino.thomas@outlook.com>
2026-07-15 15:21:34 +0700

    build: create page response wrapper

---
+++ src/main/java/com/example/e_learning_management/dto/common/PageResponse.java
@@ -0,0 +1,31 @@
+package com.example.e_learning_management.dto.common;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> items;
+
+    private Integer page;
+
+    private Integer size;
+
+    private Long totalItems;
+
+    private Integer totalPages;
+
+    private Boolean isLast;
+
+}
