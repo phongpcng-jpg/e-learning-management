54d5252c34a0bdb0b6773e1414308f6391098f50
Thomas Phong <ino.thomas@outlook.com>
2026-07-15 16:27:24 +0700

    build: add handle validation exceptions to global exception handler

--- src/main/java/com/example/e_learning_management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/e_learning_management/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.example.e_learning_management.exception;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +26,34 @@ public class GlobalExceptionHandler {
                 .body(ApiResponse.fail(Map.ofEntries(
                     Map.entry("fail", ex.getMessage())
                 )));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationExceptions(
+            MethodArgumentNotValidException ex
+    ) {
+
+        Map<String, String> errorDetailMap = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            if (error instanceof FieldError fieldError) {
+                String fieldName = fieldError.getField();
+                String defaultMessage = fieldError.getDefaultMessage();
+
+                if (fieldError.getCode() != null && fieldError.getCode().equals("typeMismatch")) {
+                    errorDetailMap.put(fieldName, CommonMessage.DATA_BINDING_EXCEPTION);
+                } else {
+                    errorDetailMap.put(fieldName, defaultMessage);
+                }
+            } else {
+                errorDetailMap.put(error.getObjectName(), error.getDefaultMessage());
+            }
+        });
+
+        return ResponseEntity.badRequest()
+                .body(ApiResponse.fail(
+                    errorDetailMap
+                ));
     }
 
     @ExceptionHandler(Exception.class)
--- src/main/java/com/example/e_learning_management/message/CommonMessage.java
+++ src/main/java/com/example/e_learning_management/message/CommonMessage.java
@@ -8,4 +8,7 @@ public final class CommonMessage {
     public static final String EXCEPTION_HANDLE =
             "Lỗi hệ thống, vui lòng tìm hiểu lại.";
 
+    public static final String DATA_BINDING_EXCEPTION = 
+            "Kiểu dữ liệu, cấu trúc không hợp lệ.";
+
 }
